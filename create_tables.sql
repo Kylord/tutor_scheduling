c3f1dd5d7695cfb4d5faee9bb950ec4ddeaacf35
aaronDubya <aaron.wolmutt@gmail.com>
2018-04-04 16:46:00 -0500

    Added tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,29 +1,54 @@
-//This is the table for courses
-DROP TABLE IF EXISTS course;
+
+
 
 --Table for course info --
+
+DROP TABLE IF EXISTS USER_T; 
+CREATE TABLE USER_T(
+	HAWKID VARCHAR(45) NOT NULL, 
+	HASHED_PSSWRD VARCHAR(45) NOT NULL, 
+	IS_ADMIN BOOLEAN NOT NULL, 
+	FIRST_NAME VARCHAR(45), 
+	LAST_NAME VARCHAR(45), 
+	EMAIL VARCHAR(45)
+	PRIMARY KEY(HAWKID)
+); 
+
+
+
+DROP TABLE IF EXISTS course;
 CREATE TABLE course (
-    course_id VARCHAR(45) NOT NULL AUTO_INCREMENT,
+    course_id VARCHAR(45) NOT NULL,
     department VARCHAR(45) NOT NULL,
     course_num INT NOT NULL,
     PRIMARY KEY (course_id)
 );
 
 /* course table */
-INSERT INTO course (course_id, department, course_num) VALUES ("1120", "Computer Science", "1120");
-INSERT INTO course (course_id, department, course_num) VALUES ("1340", "Math", "1340");
-INSERT INTO course (course_id, department, course_num) VALUES ("1440", "Physics", "1440");
+INSERT INTO course (course_id, department, course_num) VALUES ("CS1120", "Computer Science", "1120");
+INSERT INTO course (course_id, department, course_num) VALUES ("MATH1340", "Math", "1340");
+INSERT INTO course (course_id, department, course_num) VALUES ("PHYS1440", "Physics", "1440");
 
 
+DROP TABLE IF EXISTS ROLE; 
+CREATE TABLE ROLE(
+	DESCRIPTION VARCHAR(45) NOT NULL, 
+	PRIMARY KEY (HAWKID, course_id), 
+	FOREIGN KEY (HAWKID) REFERENCES USER_T(HAWKID), 
+	FOREIGN KEY(course_id) REFERENCES course(course_id)
 
-//This is the table for problem sets
+
+);
+
+
 DROP TABLE IF EXISTS probset;
 
 --Table for probset info --
 CREATE TABLE probset (
-    course_course_id VARCHAR(45) NOT NULL AUTO_INCREMENT,
-    problems VARCHAR(100000) NOT NULL,
-    FOREIGN KEY (course_course_id)
+    PROBSET_ID INT  NOT NULL AUTO_INCREMENT, 
+	TEXT VARCHAR(100000) NOT NULL,
+	PRIMARY KEY (PROBSET_ID)
+	FOREIGN KEY (course_course_id) REFERENCES course(course_id)
 );
 
 /* problem sets table */
