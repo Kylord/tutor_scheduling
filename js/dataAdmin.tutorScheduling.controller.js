f3b3d54b8d6c7595db69271b03d18d3b5e2e838a
aaronDubya <aaron.wolmutt@gmail.com>
2018-04-16 21:17:46 -0500

    Admin tables working

--- js/dataAdmin.tutorScheduling.controller.js
+++ js/dataAdmin.tutorScheduling.controller.js
@@ -4,8 +4,9 @@ myApp.controller("adminDataControl", function($scope, $http, $window) {
     $http.get('php/getusers.php')
             .then(function(response) {
                 $scope.data = response.data;
+                console.log($scope.data);
             });
-            console.log($scope.data); 
+            
             
 }); 
                 
